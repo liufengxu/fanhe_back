2a71df66d2ada6228edaa017dc0eec8f1d944a01
liufengxu <onx0320@126.com>
2015-10-07 00:48:03 +0800

    up leven-dist

--- leven.py
+++ leven.py
@@ -9,17 +9,19 @@
 description:
 author: liufengxu
 date: 2015-10-06 23:44:46
-last modified: 2015-10-07 00:12:52
+last modified: 2015-10-07 00:47:24
 version:
 """
 
 import logging
 import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
 
 
-def leven_dist(str_a, str_b):
-    len_a = len(str_a) + 1
-    len_b = len(str_b) + 1
+def levenshtein_dist(list_a, list_b):
+    len_a = len(list_a) + 1
+    len_b = len(list_b) + 1
     edit = [([0] * len_b) for i in xrange(len_a)]
     edit[0][0] = 0
     for i in xrange(1, len_a):
@@ -28,7 +30,7 @@ def leven_dist(str_a, str_b):
         edit[0][i] = i
     for i in xrange(1, len_a):
         for j in xrange(1, len_b):
-            if str_a[i - 1] == str_b[j - 1]:
+            if list_a[i - 1] == list_b[j - 1]:
                 f = 0
             else:
                 f = 1
@@ -43,13 +45,15 @@ def leven_dist(str_a, str_b):
 
 
 def main():
-    logging.basicConfig(level=logging.DEBUG, format="%(levelname)s: "
+    logging.basicConfig(level=logging.INFO, format="%(levelname)s: "
                         "%(asctime)s: %(filename)s: %(lineno)d * "
                         "%(thread)d %(message)s",
                         datefmt="%Y-%m-%d %H:%M:%S")
     s1 = sys.argv[1]
     s2 = sys.argv[2]
-    x = leven_dist(s1, s2)
+    s1 = ['我', '是', '中', '国', '人']
+    s2 = ['我', '国', '人', '口', '多']
+    x = levenshtein_dist(s1, s2)
     print x
 
 if __name__ == '__main__':
